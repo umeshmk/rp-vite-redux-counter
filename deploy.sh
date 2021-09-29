bf03735cc940b8314794afdc5726d90e95f08093
umesh kadam <umeshk149@gmail.com>
2021-09-29 20:18:13 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,13 @@ cd dist
 
 # if you are deploying to a custom domain
 echo "Deploying....."
-echo 'https://umeshmk.github.io/rp-vite-redux-counter/' > CNAME
+echo "https://umeshmk.github.io/rp-vite-redux-counter/" 
+echo "https://umeshmk.github.io/rp-vite-redux-counter/" > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
-git push -f origin  master:gh-pages
+git commit -m "deploy" &&
+
+git push -f git@github.com:umeshmk/rp-vite-redux-counter.git master:gh-pages
 
 cd -
